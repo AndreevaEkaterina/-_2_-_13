350822eb147e8757c0469c00fbcf7bc93fa12a38
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2022-10-08 16:55:21 +0800

    Решила задачу 13?

--- Program.cs
+++ Program.cs
@@ -3,4 +3,31 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 Console.Clear();
-Console.Write("Введите число:");
+Console.Write("Введите число от 0 до 99999:");
+int num = int.Parse(Console.ReadLine()!);
+if (num < 100000 && num > -1)
+{
+    if (num < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else if (num > 100 && num < 1000)
+    {
+        int a = num % 10;
+        Console.WriteLine($"{num}->{a}");
+    }
+    else if (num > 1000 && num < 10000)
+    {
+        int a = (num % 100) / 10;
+        Console.WriteLine($"{num}->{a}");
+    }
+    else
+    {
+        int a = (num % 1000) / 100;
+        Console.WriteLine($"{num}->{a}");
+    }
+}
+else
+{
+    Console.WriteLine("Некорректное число.");
+}
